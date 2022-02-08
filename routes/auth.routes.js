b238ac986fd4afa73032496bad7aa8ecaf139a21
Raúl <raul.alonso.merino@gmail.com>
2022-02-08 14:23:51 +0100

    done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,13 +5,30 @@ const bcryptjs = require('bcryptjs');
 const saltRounds = 12;
 
 const User = require('../models/User.model');
+const mongoose = require('mongoose');
+
+const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard');
  
 // GET route to display the signup form to users
-router.get('/signup', (req, res) => res.render('auth/signup'));
+router.get('/signup', isLoggedOut, (req, res) => res.render('auth/signup'));
 
 // POST route to process form data
-router.post('/signup', (req, res, next) => {
+router.post('/signup', isLoggedOut, (req, res, next) => {
     const { username, password } = req.body;
+
+    if (!username || !password){
+        res.render('auth/signup', {errorMessage: 'All fields are mandatory. Please provide username and password.'});
+        return;
+    }
+
+    // make sure passwords are strong:
+    const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!regex.test(password)) {
+        res
+        .status(500)
+        .render('auth/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
+        return;
+    };
  
     bcryptjs
         .genSalt(saltRounds)
@@ -25,12 +42,65 @@ router.post('/signup', (req, res, next) => {
         .then(() => {
             res.redirect('userProfile');
         })
+        .catch(error => {
+            if (error instanceof mongoose.Error.ValidationError) {
+                res.status(500).render('auth/signup', { errorMessage: error.message });
+            } else if (error.code === 11000){
+                res.status(500).render('auth/signup', {
+                    errorMessage: 'Username needs to be unique. Username is already used.'
+                });
+            } else {
+                next(error);
+            };
+        });
+});
+
+// GET route for login
+router.get('/login', isLoggedOut, (req, res) => res.render('auth/login'));
+
+// POST route for login
+router.post('/login', isLoggedOut, (req, res, next) => {
+    const {username, password} = req.body;
+    if (username === '' || password === ''){
+        res.render('auth/login', {errorMessage: 'Please enter both, username and password to login.'});
+        return;
+    };
+    User.findOne({username})
+        .then(user => {
+            if(!user){
+                res.render('auth/login', { errorMessage: 'Username is not registered. Try with other username.' });
+                return;
+            } else if (bcryptjs.compareSync(password, user.password)){
+                req.session.currentUser = user;
+                res.redirect('/userProfile');
+            } else {
+                res.render('auth/login', { errorMessage: 'Incorrect password.' });
+            };
+        })
         .catch(error => next(error));
 });
 
 // GET route for user profile
-router.get("/userProfile", (req, res) => {
-    res.render("users/user-profile")
+router.get("/userProfile", isLoggedIn, (req, res) => {
+    res.render("users/user-profile", {userInSession: req.session.currentUser});
+});
+
+// POST route for logout
+router.post('/logout', isLoggedIn, (req, res, next) => {
+    req.session.destroy(err => {
+        if (err) next(err);
+        res.redirect('/');
+    });
+});
+
+// GET route protected by auth
+router.get('/main', isLoggedIn, (req, res) => {
+    res.render("prot/main");
+});
+
+// GET route protected by auth
+router.get('/private', isLoggedIn, (req, res) => {
+    res.render("prot/private");
 });
  
 module.exports = router;
